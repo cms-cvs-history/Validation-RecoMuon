eefb5ac7da3b6e724e5572948b785a03c3b1f4ee
Junghwan Goh <sha1-a53d16ca32681a890c4e8971031f76fb9509968e@cern.ch>
2007-03-14 04:21:10 +0000

    Added Seed Eff, GlbMuon algo Eff

--- test/DrawEff.C
+++ test/DrawEff.C
@@ -8,7 +8,31 @@ void DrawEff(bool doFolding = false)
 
   TCanvas* c;
 
-  gNHist = 1;
+  gLineColor = 1;
+  gLegend = new TLegend(.85, .15, 1.0, .30, "");
+  c = new TCanvas("cSeedMinusEff", "Seed Minus Efficiency", 1024, 768); 
+  c->cd(); c->SetGridx(); c->SetGridy();
+  DrawEff("Mu-Pt10"  , "Seed", "Sim");
+  DrawEff("Mu-Pt50"  , "Seed", "Sim");
+  DrawEff("Mu-Pt100" , "Seed", "Sim");
+  DrawEff("Mu-Pt500" , "Seed", "Sim");
+  DrawEff("Mu-Pt1000", "Seed", "Sim");
+  gLegend->Draw("same");
+  c->Print("plots/EffSeedMinus.gif");
+                                   
+  gLineColor = 1;
+  gLegend = new TLegend(.85, .15, 1.0, .30, "");
+  c = new TCanvas("cSeedPlusEff", "Seed Plus Efficiency", 1024, 768); 
+  c->cd(); c->SetGridx(); c->SetGridy();
+  DrawEff("Mu+Pt10"  , "Seed", "Sim");
+  DrawEff("Mu+Pt50"  , "Seed", "Sim");
+  DrawEff("Mu+Pt100" , "Seed", "Sim");
+  DrawEff("Mu+Pt500" , "Seed", "Sim");
+  DrawEff("Mu+Pt1000", "Seed", "Sim");
+  gLegend->Draw("same");
+  c->Print("plots/EffSeedPlus.gif");
+ 
+  gLineColor = 1;
   gLegend = new TLegend(.85, .15, 1.0, .30, "");
   c = new TCanvas("cStaMinusEff", "StaMuon Minus Efficiency", 1024, 768); 
   c->cd(); c->SetGridx(); c->SetGridy();
@@ -20,7 +44,7 @@ void DrawEff(bool doFolding = false)
   gLegend->Draw("same");
   c->Print("plots/EffStaMinus.gif");
 
-  gNHist = 1;
+  gLineColor = 1;
   gLegend = new TLegend(.85, .15, 1.0, .30, "");
   c = new TCanvas("cStaPlusEff", "StaMuon Plus Efficiency", 1024, 768); 
   c->cd(); c->SetGridx(); c->SetGridy();
@@ -32,7 +56,7 @@ void DrawEff(bool doFolding = false)
   gLegend->Draw("same");
   c->Print("plots/EffStaPlus.gif");
 
-  gNHist = 1;
+  gLineColor = 1;
   gLegend = new TLegend(.85, .15, 1.0, .30, "");
   c = new TCanvas("cGlbMinusEff", "GlbMuon Minus Efficiency", 1024, 768); 
   c->cd(); c->SetGridx(); c->SetGridy();
@@ -44,7 +68,7 @@ void DrawEff(bool doFolding = false)
   gLegend->Draw("same");
   c->Print("plots/EffGlbMinus.gif");
  
-  gNHist = 1;
+  gLineColor = 1;
   gLegend = new TLegend(.85, .15, 1.0, .30, "");
   c = new TCanvas("cGlbPlusEff", "GlbMuon Minus Efficiency", 1024, 768); 
   c->cd(); c->SetGridx(); c->SetGridy();
@@ -56,30 +80,31 @@ void DrawEff(bool doFolding = false)
   gLegend->Draw("same");
   c->Print("plots/EffGlbPlus.gif");
 
-  gNHist = 1;
+  gLineColor = 1;
   gLegend = new TLegend(.85, .15, 1.0, .30, "");
-  c = new TCanvas("cGlbStaMinusEff", "GlbMuon/StaMuon Minus Efficiency", 1024, 768); 
+  c = new TCanvas("cGlbAlgMinusEff", "GlbMuon Minus Alg Efficiency", 1024, 768);
   c->cd(); c->SetGridx(); c->SetGridy();
-  DrawEff("Mu-Pt10"  , "Glb", "Sta");
-  DrawEff("Mu-Pt50"  , "Glb", "Sta");
-  DrawEff("Mu-Pt100" , "Glb", "Sta");
-  DrawEff("Mu-Pt500" , "Glb", "Sta");
-  DrawEff("Mu-Pt1000", "Glb", "Sta");
+  DrawEff("Mu-Pt10"  , "Glb", "Tk");
+  DrawEff("Mu-Pt50"  , "Glb", "Tk");
+  DrawEff("Mu-Pt100" , "Glb", "Tk");
+  DrawEff("Mu-Pt500" , "Glb", "Tk");
+  DrawEff("Mu-Pt1000", "Glb", "Tk");
   gLegend->Draw("same");
-  c->Print("plots/EffGlbStaMinus.gif");
-                                   
-  gNHist = 1;
+  c->Print("plots/EffGlbAlgMinus.gif");
+
+  gLineColor = 1;
   gLegend = new TLegend(.85, .15, 1.0, .30, "");
-  c = new TCanvas("cGlbStaPlusEff", "GlbMuon/StaMuon Plus Efficiency", 1024, 768); 
+  c = new TCanvas("cGlbAlgPlusEff", "GlbMuon Minus Alg Efficiency", 1024, 768);
   c->cd(); c->SetGridx(); c->SetGridy();
-  DrawEff("Mu+Pt10"  , "Glb", "Sta");
-  DrawEff("Mu+Pt50"  , "Glb", "Sta");
-  DrawEff("Mu+Pt100" , "Glb", "Sta");
-  DrawEff("Mu+Pt500" , "Glb", "Sta");
-  DrawEff("Mu+Pt1000", "Glb", "Sta");
+  DrawEff("Mu+Pt10"  , "Glb", "Tk");
+  DrawEff("Mu+Pt50"  , "Glb", "Tk");
+  DrawEff("Mu+Pt100" , "Glb", "Tk");
+  DrawEff("Mu+Pt500" , "Glb", "Tk");
+  DrawEff("Mu+Pt1000", "Glb", "Tk");
   gLegend->Draw("same");
-  c->Print("plots/EffGlbStaPlus.gif");
-  
+  c->Print("plots/EffGlbAlgPlus.gif");
+
+
 }
 
 void DrawEff(TString sampleName, TString muonType1, TString muonType2)
@@ -108,11 +133,11 @@ void DrawEff(TString sampleName, TString muonType1, TString muonType2)
   his->SetTitle(muonType1+muonType2+" Muon Efficiency");
   his->GetXaxis()->SetTitle("Psuedorapidity #eta");
   his->GetYaxis()->SetTitle("Efficiency"); 
-  his->SetLineColor(gNHist);
-  his->Draw(TString("E1X0")+(gNHist==1?"":"same"));
+  his->SetLineColor(gLineColor);
+  his->Draw(TString("E1X0")+(gLineColor==1?"":"same"));
   ConnectLines(his, sampleName);
 
-  if ( ++gNHist==kYellow ) gNHist++;
+  if ( ++gLineColor==kYellow ) gLineColor++;
   file.Close();
 }
 
