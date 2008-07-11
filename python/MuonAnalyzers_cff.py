85a98eb411fd73552a3bce6a8ae0b20ea418d5cb
cmsbuild <>
2008-07-11 14:39:30 +0000

    This commit was manufactured by cvs2git to create tag 'Validation- RecoMuon-V00-02-05-04'.
    Sprout from master 2008-05-20 10:48:03 UTC Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch> 'updates incfX'
    Cherrypick from master 2008-07-11 14:39:29 UTC Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch> 'remove importing pythin fragments provided in Configuration.StandardSequences.Geoemtry_cff. The latter should be included in all high level configuration files':
        data/GlobalMuonMatchAnalyzer.cff
        data/MuonAnalyzers.cff
        data/MuonSeedTrack.cff
        python/GlobalMuonMatchAnalyzer_cff.py
        python/MuonAnalyzers_cff.py
        python/MuonSeedTrack_cff.py
        python/RecoMuonValidators_cff.py
        test/BuildFile
    Cherrypick from master 2008-06-19 14:41:46 UTC Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch> 'removed inclusion of now obsolete MagneticField/Engine/data/volumeBasedMagneticField.cfi for 21X':
        data/RecoMuonValidators.cff

--- python/MuonAnalyzers_cff.py
+++ python/MuonAnalyzers_cff.py
@@ -1,7 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-from MagneticField.Engine.volumeBasedMagneticField_cfi import *
-from Geometry.CMSCommonData.cmsIdealGeometryXML_cfi import *
 from Geometry.CommonDetUnit.globalTrackingGeometry_cfi import *
 from RecoMuon.DetLayers.muonDetLayerGeometry_cfi import *
 from RecoMuon.TrackingTools.MuonServiceProxy_cff import *
